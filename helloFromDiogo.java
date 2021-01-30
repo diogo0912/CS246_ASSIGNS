7342bd6e9794ae1868c91f79b276e89780ae247e
Mauricio Peregrino <mauriciopssantos1@gmail.com>
2021-01-30 17:01:03 -0300

    changes

--- helloFromDiogo.java
+++ helloFromDiogo.java
@@ -4,7 +4,8 @@ public class helloFromDiogo {
         public static void main(String[] args) {
 
             System.out.println("Hello, my name is Diogo");
-            System.out.println("Hello Diogo, here is Mauricio")
+            System.out.println("Hello Diogo, here is Mauricio");
+            System.out.println("1 + 1 = 2");
         }
     }
 
